b635d32c7e0236bfa28d71194a5f358b2930acd9
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2023-08-11 14:39:04 -0600

    mongodb connection update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/AloHa", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/AloHa");
 
 module.exports = mongoose.connection;
+
+// , {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }
